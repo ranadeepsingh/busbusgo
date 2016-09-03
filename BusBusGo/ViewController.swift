827b10fd3e0539f116f5589d234aba83ed51e635
Ranadeep <ranadeepsingh96@gmail.com>
2016-09-03 15:36:06 +0530

    Initial Commit

---
+++ BusBusGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusBusGo
+//
+//  Created by Ranadeep Singh on 03/09/16.
+//  Copyright © 2016 Ranadeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
